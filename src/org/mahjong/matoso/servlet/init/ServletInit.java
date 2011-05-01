60fde6509cd92dbadc744fdf5582990f6b98603e
nicolas.pochic <nicolas.pochic@aea1c12c-1332-11df-8b85-374ad9ff7b16>
2011-05-01 15:57:15 +0000

    go home = clean attributes in session

--- src/org/mahjong/matoso/servlet/init/ServletInit.java
+++ src/org/mahjong/matoso/servlet/init/ServletInit.java
@@ -8,37 +8,51 @@
  */
 package org.mahjong.matoso.servlet.init;
 
+import java.io.IOException;
+import java.util.Enumeration;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.mahjong.matoso.constant.ServletCst;
 import org.mahjong.matoso.util.HibernateUtil;
 
 /**
  * Servlet for initializing Hibernate.<br>
  * Instanciate the factory by reading the configuration and mapping files.
- *
+ * 
  * @author ctrung
  * @date 3 juil. 2009
  */
 public class ServletInit extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	/**
 	 * Logger.
 	 */
 	private static Logger logger = Logger.getLogger(ServletInit.class);
-	
+
 	@Override
 	public void init(ServletConfig config) throws ServletException {
-		
+
 		long startTime = System.currentTimeMillis();
-		
+
 		logger.info("**************************** Initialising Hibernate...");
 		HibernateUtil.init();
 		logger.info("**************************** Hibernate successfully initialized in " + (System.currentTimeMillis() - startTime) + " ms");
 	}
-	
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		@SuppressWarnings("unchecked")
+		Enumeration<String> keys = request.getSession().getAttributeNames();
+		while (keys.hasMoreElements())
+			request.getSession().removeAttribute(keys.nextElement());
+		request.getRequestDispatcher(ServletCst.REDIRECT_TO_TOURNAMENT_LIST_SERVLET).forward(request, response);
+	}
 }
